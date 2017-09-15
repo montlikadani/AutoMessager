e1f09740bfd70e529c28ff0c82d2824917da65b8
montlikadani <montlikada@gmail.com>
2017-09-15 15:13:09 +0200

    Add files via upload

---
+++ Permissions.java
@@ -0,0 +1,15 @@
+package hu.montlikadani.AutoMessager;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+	
+	public Permission perm = new Permission("automessager.cmd.nocooldown");
+	public Permission perm1 = new Permission("automessager.plugininfo");
+	public Permission perm2 = new Permission("automessager.help");
+	public Permission perm3 = new Permission("automessager.reload");
+	public Permission perm4 = new Permission("automessager.toggle");
+	public Permission perm5 = new Permission("automessager.plugindisable");
+	public Permission perm6 = new Permission("automessager.cmd.tabcomplete");
+
+}
