6afaeee1efb439e765641ca5f418767c5c746052
montlikadani <montlikada@gmail.com>
2018-01-25 15:39:37 +0100

    Create Permissions.java

---
+++ src/hu/montlikadani/AutoMessager/Permissions.java
@@ -0,0 +1,19 @@
+package hu.montlikadani.AutoMessager;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+
+	public Permission perm1 = new Permission("automessager.plugininfo");
+	public Permission perm2 = new Permission("automessager.help");
+	public Permission perm3 = new Permission("automessager.reload");
+	public Permission perm4 = new Permission("automessager.toggle");
+	public Permission perm5 = new Permission("automessager.plugindisable");
+	public Permission perm6 = new Permission("automessager.cmd.tabcomplete");
+	public Permission perm7 = new Permission("automessager.toggle.off");
+	public Permission perm8 = new Permission("automessager.toggle.on");
+	public Permission perm9 = new Permission("automessager.broadcast");
+	public Permission perm10 = new Permission("automessager.list");
+	public Permission perm11 = new Permission("automessager.seemsg");
+
+}
