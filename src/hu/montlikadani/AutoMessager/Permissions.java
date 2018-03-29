301876273636aed68f60c94a8fbcb6826e542490
montlikadani <montlikada@gmail.com>
2018-03-29 12:13:00 +0200

    Added some multiple permission

--- src/hu/montlikadani/AutoMessager/Permissions.java
+++ src/hu/montlikadani/AutoMessager/Permissions.java
@@ -4,16 +4,17 @@ import org.bukkit.permissions.Permission;
 
 public class Permissions {
 
-	public Permission perm1 = new Permission("automessager.plugininfo");
-	public Permission perm2 = new Permission("automessager.help");
-	public Permission perm3 = new Permission("automessager.reload");
-	public Permission perm4 = new Permission("automessager.toggle");
-	public Permission perm5 = new Permission("automessager.plugindisable");
-	public Permission perm6 = new Permission("automessager.cmd.tabcomplete");
-	public Permission perm7 = new Permission("automessager.toggle.off");
-	public Permission perm8 = new Permission("automessager.toggle.on");
-	public Permission perm9 = new Permission("automessager.broadcast");
-	public Permission perm10 = new Permission("automessager.list");
-	public Permission perm11 = new Permission("automessager.seemsg");
+	public static final Permission PINFO = new Permission("automessager.plugininfo");
+	public static final Permission HELP = new Permission("automessager.help");
+	public static final Permission RELOAD = new Permission("automessager.reload");
+	public static final Permission PDISABLE = new Permission("automessager.plugindisable");
+	public static final Permission TABCOMP = new Permission("automessager.cmd.tabcomplete");
+	public static final Permission TOGGLE = new Permission("automessager.toggle");
+	public static final Permission TOGGLEOFF = new Permission("automessager.toggle.off");
+	public static final Permission TOGGLEON = new Permission("automessager.toggle.on");
+	public static final Permission BC = new Permission("automessager.broadcast");
+	public static final Permission LIST = new Permission("automessager.list");
+	public static final Permission SEEMSG = new Permission("automessager.seemsg");
+	public static final Permission ADD = new Permission("automessager.add");
 
 }
